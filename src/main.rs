bf0dfa9b8d6e0557af5431b4250fb43c84aa87b0
Evan Relf <evan@evanrelf.com>
2025-09-19 11:37:58 -0700

    Use `camino`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use camino::Utf8PathBuf;
 use clap::Parser as _;
 use etcetera::app_strategy::{AppStrategy as _, AppStrategyArgs, Xdg};
 use tokio::fs;
@@ -9,17 +10,17 @@ struct Args {}
 async fn main() -> anyhow::Result<()> {
     let _args = Args::parse();
 
+    tracing_subscriber::fmt::init();
+
     let xdg = Xdg::new(AppStrategyArgs {
         top_level_domain: String::from("com"),
         author: String::from("Evan Relf"),
         app_name: String::from("Be"),
     })?;
 
-    let xdg_cache_dir = xdg.cache_dir();
+    let cache_dir = Utf8PathBuf::try_from(xdg.cache_dir())?;
 
-    fs::create_dir_all(&xdg_cache_dir).await?;
-
-    tracing_subscriber::fmt::init();
+    fs::create_dir_all(&cache_dir).await?;
 
     tracing::info!("Hello, world!");
 
