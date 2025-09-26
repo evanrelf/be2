66e463639245d2b3660c7b70eed1f164fe89f295
Evan Relf <evan@evanrelf.com>
2025-09-26 15:33:50 -0700

    Dismiss some warnings

--- src/format.rs
+++ src/format.rs
@@ -1,5 +1,6 @@
 use crate::cli::format::Args;
 
+#[expect(clippy::unused_async)]
 pub async fn run(_args: Args) -> anyhow::Result<()> {
     tracing::info!("totally formatting right now...");
 
--- src/lint.rs
+++ src/lint.rs
@@ -1,5 +1,6 @@
 use crate::cli::lint::Args;
 
+#[expect(clippy::unused_async)]
 pub async fn run(_args: Args) -> anyhow::Result<()> {
     tracing::info!("totally linting right now...");
 
