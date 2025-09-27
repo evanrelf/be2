e631ea3e60b705e6588d2ffa16d93d5df0937b24
Evan Relf <evan@evanrelf.com>
2025-09-26 20:10:17 -0700

    Implement `construct` and a bunch of related shit

--- src/build.rs
+++ src/build.rs
@@ -5,7 +5,7 @@ use serde::{Deserialize, Serialize};
 use sqlx::SqlitePool;
 use std::{
     collections::{HashMap, HashSet},
-    hash::BuildHasherDefault,
+    hash::{BuildHasherDefault, Hash as _, Hasher as _},
     str,
     sync::Arc,
 };
@@ -19,39 +19,24 @@ pub enum Key {
     ReadFile(Utf8PathBuf),
 }
 
-#[derive(Clone, Deserialize, Serialize)]
+#[derive(Clone, Deserialize, Eq, Hash, PartialEq, Serialize)]
 pub enum Value {
     Path(Utf8PathBuf),
     Bytes(Bytes),
+}
+
+impl Value {
+    fn xxhash(&self) -> u64 {
+        let mut hasher = XxHash3_64::default();
+        self.hash(&mut hasher);
+        hasher.finish()
+    }
 }
 
 pub struct Trace {
     pub key: Key,
     pub deps: HashMap<Key, u64, BuildHasherDefault<XxHash3_64>>,
     pub value: Value,
-}
-
-impl TryFrom<Trace> for db::Trace {
-    type Error = anyhow::Error;
-    fn try_from(trace: Trace) -> Result<Self, Self::Error> {
-        let mut key_buf = Vec::new();
-        ciborium::into_writer(&trace.key, &mut key_buf)?;
-        let key = Bytes::from(key_buf);
-
-        let mut deps = HashMap::default();
-        for (dep_key, dep_value_hash) in trace.deps {
-            let mut dep_key_buf = Vec::new();
-            ciborium::into_writer(&dep_key, &mut dep_key_buf)?;
-            let dep_key = Bytes::from(dep_key_buf);
-            deps.insert(dep_key, dep_value_hash);
-        }
-
-        let mut value_buf = Vec::new();
-        ciborium::into_writer(&trace.value, &mut value_buf)?;
-        let value = Bytes::from(value_buf);
-
-        Ok(db::Trace { key, deps, value })
-    }
 }
 
 pub struct Context {
@@ -91,13 +76,98 @@ impl Context {
     }
 
     async fn record(&mut self, trace: Trace) -> anyhow::Result<()> {
-        let db_trace = db::Trace::try_from(trace)?;
-        db::insert_trace(&self.db, &db_trace).await?;
+        insert_trace(&self.db, trace).await?;
+
         Ok(())
     }
 
-    #[expect(clippy::unused_async)]
-    async fn construct(&mut self, _key: &Key) -> anyhow::Result<HashSet<Value>> {
-        todo!()
+    async fn construct(&mut self, key: &Key) -> anyhow::Result<HashSet<Value>> {
+        let traces = fetch_traces(&self.db, Some(key)).await?;
+
+        let mut matches = HashSet::new();
+
+        'trace: for trace in traces {
+            if trace.key != *key {
+                continue 'trace;
+            }
+
+            for (dep_key, dep_value_hash) in trace.deps {
+                if dep_value_hash != self.build(&dep_key).await?.xxhash() {
+                    continue 'trace;
+                }
+            }
+
+            matches.insert(trace.value);
+        }
+
+        Ok(matches)
     }
+}
+
+impl TryFrom<Trace> for db::Trace {
+    type Error = anyhow::Error;
+    fn try_from(trace: Trace) -> Result<Self, Self::Error> {
+        let mut key_buf = Vec::new();
+        ciborium::into_writer(&trace.key, &mut key_buf)?;
+        let key = Bytes::from(key_buf);
+
+        let mut deps = HashMap::default();
+        for (dep_key, dep_value_hash) in trace.deps {
+            let mut dep_key_buf = Vec::new();
+            ciborium::into_writer(&dep_key, &mut dep_key_buf)?;
+            let dep_key = Bytes::from(dep_key_buf);
+            deps.insert(dep_key, dep_value_hash);
+        }
+
+        let mut value_buf = Vec::new();
+        ciborium::into_writer(&trace.value, &mut value_buf)?;
+        let value = Bytes::from(value_buf);
+
+        Ok(db::Trace { key, deps, value })
+    }
+}
+
+impl TryFrom<db::Trace> for Trace {
+    type Error = anyhow::Error;
+    fn try_from(db_trace: db::Trace) -> Result<Self, Self::Error> {
+        let key = ciborium::from_reader(&db_trace.key[..])?;
+
+        let mut deps = HashMap::default();
+        for (db_dep_key, dep_value_hash) in db_trace.deps {
+            let dep_key = ciborium::from_reader(&db_dep_key[..])?;
+            deps.insert(dep_key, dep_value_hash);
+        }
+
+        let value = ciborium::from_reader(&db_trace.value[..])?;
+
+        Ok(Trace { key, deps, value })
+    }
+}
+
+async fn fetch_traces(db: &SqlitePool, key: Option<&Key>) -> anyhow::Result<Vec<Trace>> {
+    let db_key = if let Some(key) = key {
+        let mut db_key_buf = Vec::new();
+        ciborium::into_writer(key, &mut db_key_buf)?;
+        let db_key = Bytes::from(db_key_buf);
+        Some(db_key)
+    } else {
+        None
+    };
+
+    let db_traces = db::fetch_traces(db, db_key.as_ref()).await?;
+
+    let mut traces = Vec::with_capacity(db_traces.len());
+
+    for db_trace in db_traces {
+        let trace = Trace::try_from(db_trace)?;
+        traces.push(trace);
+    }
+
+    Ok(traces)
+}
+
+async fn insert_trace(db: &SqlitePool, trace: Trace) -> anyhow::Result<i64> {
+    let db_trace = db::Trace::try_from(trace)?;
+    let trace_id = db::insert_trace(db, &db_trace).await?;
+    Ok(trace_id)
 }
