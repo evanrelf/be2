b2bbf6ca17773047468fd64e9b612f042aeff8a8
Evan Relf <evan@evanrelf.com>
2025-09-30 21:58:24 -0700

    Move all CLI modules into the same file

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,3 @@
-pub mod format;
-pub mod lint;
-
 #[derive(clap::Parser)]
 #[command(disable_help_subcommand = true)]
 pub struct Args {
@@ -18,4 +15,72 @@ pub enum Command {
 
     /// Delete cache
     Clean,
+}
+
+pub mod format {
+    use camino::Utf8PathBuf;
+
+    #[derive(clap::Args)]
+    pub struct Args {
+        #[command(subcommand)]
+        pub command: Option<Command>,
+    }
+
+    #[derive(clap::Subcommand)]
+    pub enum Command {
+        /// Format Haskell code
+        Haskell(HaskellArgs),
+
+        /// Format Nix code
+        Nix(NixArgs),
+    }
+
+    #[derive(clap::Args, Default)]
+    pub struct HaskellArgs {
+        /// Only format specific paths
+        #[arg(group = "input")]
+        pub paths: Vec<Utf8PathBuf>,
+
+        /// Format code piped to `stdin`
+        #[arg(long, group = "input")]
+        pub stdin: bool,
+    }
+
+    #[derive(clap::Args, Default)]
+    pub struct NixArgs {
+        /// Only format specific paths
+        #[arg(group = "input")]
+        pub paths: Vec<Utf8PathBuf>,
+
+        /// Format code piped to `stdin`
+        #[arg(long, group = "input")]
+        pub stdin: bool,
+    }
+}
+
+pub mod lint {
+    use camino::Utf8PathBuf;
+
+    #[derive(clap::Args)]
+    pub struct Args {
+        #[command(subcommand)]
+        pub command: Option<Command>,
+    }
+
+    #[derive(clap::Subcommand)]
+    pub enum Command {
+        /// Lint Haskell code
+        Haskell(HaskellArgs),
+    }
+
+    #[derive(clap::Args, Default)]
+    pub struct HaskellArgs {
+        /// Only lint specific paths
+        #[arg(group = "input")]
+        pub paths: Vec<Utf8PathBuf>,
+
+        /// Lint code piped to `stdin`
+        #[arg(long, group = "input")]
+        pub stdin: bool,
+    }
 }
