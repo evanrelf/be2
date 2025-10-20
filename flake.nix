b1a3296cd17184a05a10bd99100da7988ba3f3e6
Evan Relf <evan@evanrelf.com>
2025-10-20 11:10:30 -0700

    Set up Haskell project

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "be2";
+
+  inputs = {
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    ghciwatch-compat = {
+      url = "github:evanrelf/ghciwatch-compat";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+
+      perSystem = { config, inputs', pkgs, system, ... }: {
+        _module.args.pkgs =
+          import inputs.nixpkgs {
+            localSystem = system;
+            overlays = [
+              inputs.ghciwatch-compat.overlays.default
+            ];
+          };
+
+        packages = {
+          default = config.packages.be2;
+
+          be2 = pkgs.haskellPackages.callCabal2nix "be2" ./. { };
+        };
+
+        devShells.default =
+          pkgs.mkShell {
+            inputsFrom = [ config.packages.be2.env ];
+            packages = [
+              pkgs.cabal-install
+              pkgs.ghciwatch-compat-ghcid
+              pkgs.nixpkgs-fmt
+            ];
+          };
+      };
+    };
+}
---
+++ src/Main.hs
@@ -0,0 +1,6 @@
+module Main (main) where
+
+import Be qualified
+
+main :: IO ()
+main = Be.main
