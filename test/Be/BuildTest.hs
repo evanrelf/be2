d98baeaacb9d07862823018ec1c668a16ddccc3a
Evan Relf <evan@evanrelf.com>
2025-11-06 22:08:43 -0800

    Drop required proxies

--- test/Be/BuildTest.hs
+++ test/Be/BuildTest.hs
@@ -188,13 +188,13 @@ unit_existential_build_system = do
     let tasks :: TaskContext' -> SomeValue -> IO (SomeValue, Bool)
         tasks taskContext someValue
           | Just (Add1Key (Identity n)) <- fromSomeValue someValue = do
-              m <- taskBuild (Proxy @Add1) taskContext n
+              m <- taskBuild @Add1 taskContext n
               let value = toSomeValue (Add1Value m)
               let volatile = False
               pure (value, volatile)
 
           | Just (GreetKey (Identity name)) <- fromSomeValue someValue = do
-              greeting <- taskBuild (Proxy @Greet) taskContext name
+              greeting <- taskBuild @Greet taskContext name
               let value = toSomeValue (GreetValue greeting)
               let volatile = False
               pure (value, volatile)
